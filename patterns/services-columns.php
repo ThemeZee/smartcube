00d8ede4c15ab464b816ab0bbc374a9bc70c8b12
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-08-10 14:54:25 +0200

    initial commit

---
+++ patterns/services-columns.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Title: Services Columns
+ * Slug: smartcube/services-columns
+ * Categories: smartcube_services
+*/
+?>
+
+<!-- wp:group {"style":{"spacing":{"margin":{"bottom":"4vw"}}}} -->
+<div class="wp-block-group" style="margin-bottom:4vw">
+
+	<!-- wp:heading -->
+	<h2><?php _e( 'What we offer', 'smartcube' ); ?></h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:paragraph {"fontSize":"large"} -->
+	<p class="has-large-font-size">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:columns {"style":{"spacing":{"blockGap":"2rem"}}} -->
+	<div class="wp-block-columns">
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+
+			<!-- wp:heading {"level":3} -->
+			<h3><?php _e( 'Service One', 'smartcube' ); ?></h3>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+			<!-- /wp:paragraph -->
+
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+
+			<!-- wp:heading {"level":3} -->
+			<h3><?php _e( 'Service Two', 'smartcube' ); ?></h3>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+			<!-- /wp:paragraph -->
+
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+
+			<!-- wp:heading {"level":3} -->
+			<h3><?php _e( 'Service Three', 'smartcube' ); ?></h3>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+			<!-- /wp:paragraph -->
+
+		</div>
+		<!-- /wp:column -->
+
+	</div>
+	<!-- /wp:columns -->
+
+</div>
+<!-- /wp:group -->
