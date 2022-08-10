0f5eef71a5dd8fa9771a8bfd7cc91c6d3b14e1f1
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-08-10 16:52:29 +0200

    adjusted sidebar block template

--- patterns/sidebar-widgets.php
+++ patterns/sidebar-widgets.php
@@ -6,38 +6,38 @@
 */
 ?>
 
-<!-- wp:group {"className":"widget"} -->
-<div class="wp-block-group widget">
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"1.5rem","right":"1.5rem","bottom":"1.5rem","left":"1.5rem"}}},"backgroundColor":"white","className":"widget"} -->
+<div class="wp-block-group widget has-white-background-color has-background" style="padding-top:1.5rem;padding-right:1.5rem;padding-bottom:1.5rem;padding-left:1.5rem">
 
-	<!-- wp:heading {"fontSize":"large"} -->
-	<h2 class="has-large-font-size"><?php _e( 'Categories', 'smartcube' ); ?></h2>
+	<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"className":"is-style-sidebar-title","fontSize":"normal"} -->
+	<h2 class="is-style-sidebar-title has-normal-font-size" style="font-style:normal;font-weight:400"><?php _e( 'Categories', 'smartcube' ); ?></h2>
 	<!-- /wp:heading -->
 
-	<!-- wp:categories {"className":"is-style-flip-link-hover"} /-->
+	<!-- wp:categories /-->
 
 </div>
 <!-- /wp:group -->
 
-<!-- wp:group {"className":"widget"} -->
-<div class="wp-block-group widget">
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"1.5rem","right":"1.5rem","bottom":"1.5rem","left":"1.5rem"}}},"backgroundColor":"white","className":"widget"} -->
+<div class="wp-block-group widget has-white-background-color has-background" style="padding-top:1.5rem;padding-right:1.5rem;padding-bottom:1.5rem;padding-left:1.5rem">
 
-	<!-- wp:heading {"fontSize":"large"} -->
-	<h2 class="has-large-font-size"><?php _e( 'Archives', 'smartcube' ); ?></h2>
+	<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"className":"is-style-sidebar-title","fontSize":"normal"} -->
+	<h2 class="is-style-sidebar-title has-normal-font-size" style="font-style:normal;font-weight:400"><?php _e( 'Archives', 'smartcube' ); ?></h2>
 	<!-- /wp:heading -->
 
-	<!-- wp:archives {"className":"is-style-flip-link-hover"} /-->
+	<!-- wp:archives /-->
 
 </div>
 <!-- /wp:group -->
 
-<!-- wp:group {"className":"widget"} -->
-<div class="wp-block-group widget">
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"1.5rem","right":"1.5rem","bottom":"1.5rem","left":"1.5rem"}}},"backgroundColor":"white","className":"widget"} -->
+<div class="wp-block-group widget has-white-background-color has-background" style="padding-top:1.5rem;padding-right:1.5rem;padding-bottom:1.5rem;padding-left:1.5rem">
 
-	<!-- wp:heading {"fontSize":"large"} -->
-	<h2 class="has-large-font-size"><?php _e( 'Tags', 'smartcube' ); ?></h2>
+	<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"className":"is-style-sidebar-title","fontSize":"normal"} -->
+	<h2 class="is-style-sidebar-title has-normal-font-size" style="font-style:normal;font-weight:400"><?php _e( 'Tags', 'smartcube' ); ?></h2>
 	<!-- /wp:heading -->
 
-	<!-- wp:tag-cloud {"className":"is-style-flip-link-hover"} /-->
+	<!-- wp:tag-cloud /-->
 
 </div>
 <!-- /wp:group -->
