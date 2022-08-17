85d2adedd79d3d2808ec5ca5885f1201cb6fbf12
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-08-17 11:39:10 +0200

    updated Portfolio block patterns

--- patterns/portfolio-columns.php
+++ patterns/portfolio-columns.php
@@ -6,96 +6,42 @@
 */
 ?>
 
-<!-- wp:group {"style":{"spacing":{"margin":{"bottom":"4vw"}}}} -->
-<div class="wp-block-group" style="margin-bottom:4vw">
+<!-- wp:columns {"verticalAlignment":"center"} -->
+<div class="wp-block-columns are-vertically-aligned-center">
 
-	<!-- wp:columns {"style":{"spacing":{"blockGap":"4vw"}}} -->
-	<div class="wp-block-columns">
+	<!-- wp:column {"verticalAlignment":"center","width":""} -->
+	<div class="wp-block-column is-vertically-aligned-center">
 
-		<!-- wp:column {"width":"40%"} -->
-		<div class="wp-block-column" style="flex-basis:40%">
+		<!-- wp:heading -->
+		<h2><?php _e( 'Project', 'smartcube' ); ?></h2>
+		<!-- /wp:heading -->
 
-			<!-- wp:heading -->
-			<h2><?php _e( 'What we have done', 'smartcube' ); ?></h2>
-			<!-- /wp:heading -->
+		<!-- wp:paragraph -->
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+		<!-- /wp:paragraph -->
 
-			<!-- wp:paragraph {"fontSize":"large"} -->
-			<p class="has-large-font-size">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-			<!-- /wp:paragraph -->
-
+		<!-- wp:buttons -->
+		<div class="wp-block-buttons">
+			<!-- wp:button -->
+			<div class="wp-block-button"><a class="wp-block-button__link"><?php _e( 'Learn more', 'smartcube' ); ?></a></div>
+			<!-- /wp:button -->
 		</div>
-		<!-- /wp:column -->
-
-		<!-- wp:column {"width":"60%"} -->
-		<div class="wp-block-column" style="flex-basis:60%">
-
-			<!-- wp:columns {"style":{"spacing":{"blockGap":"2rem"}}} -->
-			<div class="wp-block-columns">
-
-				<!-- wp:column -->
-				<div class="wp-block-column">
-
-					<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-					<figure class="wp-block-image size-full is-style-default">
-						<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/default-image.png" alt="">
-					</figure>
-					<!-- /wp:image -->
-
-					<!-- wp:heading {"level":3} -->
-					<h3><?php _e( 'Project One', 'smartcube' ); ?></h3>
-					<!-- /wp:heading -->
-
-					<!-- wp:paragraph -->
-					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
-					<!-- /wp:paragraph -->
-
-					<!-- wp:buttons -->
-					<div class="wp-block-buttons">
-						<!-- wp:button -->
-						<div class="wp-block-button"><a class="wp-block-button__link"><?php _e( 'Learn more', 'smartcube' ); ?></a></div>
-						<!-- /wp:button -->
-					</div>
-					<!-- /wp:buttons -->
-
-				</div>
-				<!-- /wp:column -->
-
-				<!-- wp:column -->
-				<div class="wp-block-column">
-
-					<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-					<figure class="wp-block-image size-full is-style-default">
-						<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/default-image.png" alt="">
-					</figure>
-					<!-- /wp:image -->
-
-					<!-- wp:heading {"level":3} -->
-					<h3><?php _e( 'Project Two', 'smartcube' ); ?></h3>
-					<!-- /wp:heading -->
-
-					<!-- wp:paragraph -->
-					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</p>
-					<!-- /wp:paragraph -->
-
-					<!-- wp:buttons -->
-					<div class="wp-block-buttons">
-						<!-- wp:button -->
-						<div class="wp-block-button"><a class="wp-block-button__link"><?php _e( 'Learn more', 'smartcube' ); ?></a></div>
-						<!-- /wp:button -->
-					</div>
-					<!-- /wp:buttons -->
-
-				</div>
-				<!-- /wp:column -->
-
-			</div>
-			<!-- /wp:columns -->
-
-		</div>
-		<!-- /wp:column -->
+		<!-- /wp:buttons -->
 
 	</div>
-	<!-- /wp:columns -->
+	<!-- /wp:column -->
+
+	<!-- wp:column {"verticalAlignment":"center","width":""} -->
+	<div class="wp-block-column is-vertically-aligned-center">
+
+		<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+		<figure class="wp-block-image size-full is-style-default">
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/default-image.png" alt="">
+		</figure>
+		<!-- /wp:image -->
+
+	</div>
+	<!-- /wp:column -->
 
 </div>
-<!-- /wp:group -->
+<!-- /wp:columns -->
