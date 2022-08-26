47b6ac74cb0ac86c4726fb53da860565e69c9f95
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-08-26 11:37:28 +0200

    updated Call to Action block pattern

--- patterns/call-to-action-vertical.php
+++ patterns/call-to-action-vertical.php
@@ -9,15 +9,15 @@
 <!-- wp:group {"backgroundColor":"light-gray","layout":{"inherit":false}} -->
 <div class="wp-block-group has-light-gray-background-color has-background">
 
-	<!-- wp:heading {"textAlign":"center"} -->
-	<h2 class="has-text-align-center"><?php _e( 'Call to Action', 'smartcube' ); ?></h2>
+	<!-- wp:heading -->
+	<h2><?php _e( 'Call to Action', 'smartcube' ); ?></h2>
 	<!-- /wp:heading -->
 
-	<!-- wp:paragraph {"align":"center"} -->
-	<p class="has-text-align-center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis diss parturient.</p>
+	<!-- wp:paragraph -->
+	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis diss parturient.</p>
 	<!-- /wp:paragraph -->
 
-	<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+	<!-- wp:buttons -->
 	<div class="wp-block-buttons">
 		<!-- wp:button -->
 		<div class="wp-block-button"><a class="wp-block-button__link"><?php _e( 'Learn more', 'smartcube' ); ?></a></div>
